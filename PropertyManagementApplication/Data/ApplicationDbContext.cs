0626b31f665ab9c55b4e6e4f8209171c381f2189
Hunter <102537986+HunterBarney@users.noreply.github.com>
2026-09-18 00:47:44 -0400

    Initial Commit & user setup

---
+++ PropertyManagementApplication/Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PropertyManagementApplication.Models;
+
+namespace PropertyManagementApplication.Data;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+}
